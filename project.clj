d289ecfa0f053ed1858085c4b7e602df5e0ebf10
Simon Lomax <simax.99.99@gmail.com>
2014-05-08 08:00:59 +0100

    Fixed project.clj file lein-lobos was not described with a quoted version number

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:user {:dependencies [[lein-lobos 1.0.0-beta1]]}}
+  :profiles {:user {:dependencies [[lein-lobos "1.0.0-beta1"]]}}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lobos "1.0.0-beta1"]
                  [korma "0.3.0-RC5"]
