0deb51ee170159d8c9cc9ac151c66a7265a5a75c
Simon Lomax <simax.99.99@gmail.com>
2014-05-09 17:26:39 +0100

    Removed some unused lobos files

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [korma "0.3.0-RC5"]
-                 [postgresql/postgresql "9.3-1100-jdbc4"]]
+                 [postgresql/postgresql "9.3-1100-jdbc4"]
+                 [liberator "0.11.0"]]
   :clj-sql-up {:database {:subprotocol "postgresql"
                           :subname "mytime"
                           :user "simonlomax"
